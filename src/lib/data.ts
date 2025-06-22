1a7397b62789dafb1fcb70ea35a3ab01ea2712f0
Goutham Raja <goutham2135@gmail.com>
2025-06-22 17:01:24 +0000

    add these reference images inside 1/2 Kg Cake reference images https://

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -10,10 +10,25 @@ const bentoCakeReferenceImages: Option[] = [
     { name: 'Style 7', image: 'https://i.postimg.cc/C5TCVXWh/Whats-App-Image-2025-06-22-at-8.jpg' },
 ];
 
+const halfKgCakeReferenceImages: Option[] = [
+    { name: 'Style 1', image: 'https://i.postimg.cc/KzJNtJVx/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 2', image: 'https://i.postimg.cc/440vZ7RL/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 3', image: 'https://i.postimg.cc/sfHmXH7J/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 4', image: 'https://i.postimg.cc/NFK1mYmk/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 5', image: 'https://i.postimg.cc/Z5pxwTgf/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 6', image: 'https://i.postimg.cc/3NNZ125p/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 7', image: 'https://i.postimg.cc/fR9B173f/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 8', image: 'https://i.postimg.cc/FsJyKr99/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 9', image: 'https://i.postimg.cc/BQ8g15Cf/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 10', image: 'https://i.postimg.cc/283H7NrJ/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 11', image: 'https://i.postimg.cc/bvPHM7fq/Whats-App-Image-2025-06-22-at-8.jpg' },
+    { name: 'Style 12', image: 'https://i.postimg.cc/3NqFcWYq/Whats-App-Image-2025-06-22-at-8.jpg' },
+];
+
 export const bakeryItems: BakeryItem[] = [
     // Cakes
     { id: 1, name: "Bento Cake", description: "A cute, personal-sized bento cake.", originalPrice: 300, discountedPrice: 250, discount: 17, image: "https://i.postimg.cc/1zXr0pGQ/Whats-App-Image-2025-06-22-at-8.jpg", category: "Cakes", aiHint: "bento cake", referenceImages: bentoCakeReferenceImages },
-    { id: 2, name: "1/2 Kg Cake", description: "Half a kilogram of delicious cake.", originalPrice: 600, discountedPrice: 500, discount: 17, image: "https://i.postimg.cc/KzJNtJVx/Whats-App-Image-2025-06-22-at-8.jpg", category: "Cakes", aiHint: "half kg cake" },
+    { id: 2, name: "1/2 Kg Cake", description: "Half a kilogram of delicious cake.", originalPrice: 600, discountedPrice: 500, discount: 17, image: "https://i.postimg.cc/KzJNtJVx/Whats-App-Image-2025-06-22-at-8.jpg", category: "Cakes", aiHint: "half kg cake", referenceImages: halfKgCakeReferenceImages },
     { id: 17, name: "1 Kg Cake", description: "A full kilogram of cake for your celebration.", originalPrice: 1200, discountedPrice: 1000, discount: 17, image: "https://placehold.co/100x100.png", category: "Cakes", aiHint: "one kg cake" },
     { id: 18, name: "Dry Cake", description: "A classic dry cake, perfect with tea.", originalPrice: 400, discountedPrice: 350, discount: 13, image: "https://placehold.co/100x100.png", category: "Cakes", aiHint: "dry cake" },
 

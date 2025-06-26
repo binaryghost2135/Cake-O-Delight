598e084e616eb38fa547cc27138d2710a9cb0a5d
Goutham Raja <goutham2135@gmail.com>
2025-06-26 12:50:05 +0000

    and add these images in "More Sweet Delights" https://i.postimg.cc/JtVSV

--- src/components/scroll-animated-grid.tsx
+++ src/components/scroll-animated-grid.tsx
@@ -5,12 +5,11 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 const collagePhotos = [
-  { src: "https://placehold.co/600x400.png", hint: "chocolate velvet cake" },
-  { src: "https://placehold.co/600x400.png", hint: "strawberry shortcake" },
-  { src: "https://placehold.co/600x400.png", hint: "lemon meringue pie" },
-  { src: "https://placehold.co/600x400.png", hint: "golden honeycomb cake" },
-  { src: "https://placehold.co/600x400.png", hint: "blueberry cheesecake" },
-  { src: "https://placehold.co/600x400.png", hint: "red velvet cupcake" },
+  { src: "https://i.postimg.cc/JtVSVxLw/Whats-App-Image-2025-06-23-at-1.jpg", hint: "chocolate cake" },
+  { src: "https://i.postimg.cc/MG375cTk/Whats-App-Image-2025-06-23-at-11.jpg", hint: "cake layers" },
+  { src: "https://i.postimg.cc/nzSKs5yt/Whats-App-Image-2025-06-23-at-7.jpg", hint: "decorated cake" },
+  { src: "https://i.postimg.cc/wBm35rRp/Whats-App-Image-2025-06-23-at-7.jpg", hint: "pink cake" },
+  { src: "https://i.postimg.cc/3Nmxm0kF/Whats-App-Image-2025-06-23-at-7.jpg", hint: "pastry design" },
 ];
 
 export function ScrollAnimatedGrid() {
@@ -26,7 +25,7 @@ export function ScrollAnimatedGrid() {
           if (entry.isIntersecting) {
             entry.target.classList.add("is-visible");
           } else {
-            entry.target.classList.remove("is-visible");
+            // entry.target.classList.remove("is-visible");
           }
         });
       },
